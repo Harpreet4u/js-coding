3a4dfbf23277077cd279eae03069213f4c980ab9
harpreet4u <happysinghsingh7@gmail.com>
2020-12-01 22:22:12 +1100

    quicksort fix

--- quickSort.js
+++ quickSort.js
@@ -3,15 +3,14 @@
 // Complexity O(nlogn) for avenrage case worst case is O(n^2)
 
 const quickSort = (data, left, right) => {
-    if (left <= right && right-left <= 1) {
-        return data;
-    }    
-    const pivot = partition(data, left, right);
-    if (left < pivot - 1) {
-        quickSort(data, left, pivot-1);
-    }
-    if (pivot < right) {
-        quickSort(data, pivot, right);
+    if (left < right) { 
+        const pivot = partition(data, left, right);
+        if (left < pivot - 1) {
+            quickSort(data, left, pivot-1);
+        }
+        if (pivot < right) {
+            quickSort(data, pivot, right);
+        }
     }
     return data;
 };
@@ -20,17 +19,17 @@ const partition = (data, left, right) => {
     const mid = Math.floor((left+right)/2);
 
     const pivot = data[mid];
-    while (left < right) {
+    while (left <= right) {
         while (data[left] < pivot) {
             left++;
         }
         while(data[right] > pivot) {
             right--;
         }
-        if (left < right) {
-            data[left] = data[left] + data[right];
-            data[right] = data[left] - data[right];
-            data[left] = data[left] - data[right];
+        if (left <= right) {
+            const temp = data[left];
+            data[left] = data[right];
+            data[right] = temp;
             left++;
             right--;
         }
@@ -38,8 +37,25 @@ const partition = (data, left, right) => {
     return left;
 };
 
-const data = [];
-for (let i=0;i<10;++i) {
-    data.push(Math.floor(Math.random()*100));
+const verifySort = (res) => {
+
+    for (let i=0;i<res.length;++i) {
+        for (let j=i+1;j<res.length;++j) {
+            if (res[i] > res[j]) {
+                console.log("Not sorted correctly. " + res);
+            }
+        }
+    }
+};
+
+
+for (let i=0;i<10000;++i) {
+    const data = [];
+    for (let i=0;i<Math.floor(Math.random()*1000);++i) {
+        data.push(Math.floor(Math.random()*10000));
+    }
+    const res = quickSort(data, 0, data.length-1);
+    verifySort(res);
 }
-console.log(quickSort(data, 0, data.length-1));
+
+console.log("All good!");
